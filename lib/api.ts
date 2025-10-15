a0c70564a77a4058b22246db33b54a4ac13b99a0
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2025-10-16 01:11:45 +0330

    fix(refresh): Change api flow and remove queue and fix refresh api problem

--- lib/api.ts
+++ lib/api.ts
@@ -1,64 +1,29 @@
 import axios, { AxiosError, AxiosRequestConfig } from "axios";
 
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: BASE_URL,
   withCredentials: true,
 });
 
-let isRefreshing = false;
-let queue: {
-  resolve: (value?: unknown) => void;
-  reject: (err: any) => void;
-}[] = [];
-
-const processQueue = (error: unknown, tokenRefreshed = false) => {
-  queue.forEach((prom) => {
-    if (error) {
-      prom.reject(error);
-    } else {
-      prom.resolve(tokenRefreshed);
-    }
-  });
-
-  queue = [];
-};
-
 api.interceptors.response.use(
   (res) => res,
-  async (error: AxiosError) => {
-    const originalRequest = error.config as AxiosRequestConfig & {
+  async (err: AxiosError) => {
+    const originalConfig = err.config as AxiosRequestConfig & {
       _retry?: boolean;
     };
-
-    // if we got 401 and we haven't refreshed yet
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      // if a req is refreshing , we must wait
-      if (isRefreshing) {
-        return new Promise((resolve, reject) => {
-          queue.push({
-            resolve: () => resolve(api(originalRequest)),
-            reject,
-          });
-        });
-      }
-
-      isRefreshing = true;
-
+    if (err.response?.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true;
       try {
-        await api.get("/v1/auth/refresh-token");
-        processQueue(null);
-        return api(originalRequest); // retry the req
-      } catch (err) {
-        processQueue(err);
-        window.location.href = "/login";
-        return Promise.reject(err);
-      } finally {
-        isRefreshing = false;
+        const { data } = await axios.get(`${BASE_URL}v1/auth/refresh`, {
+          withCredentials: true,
+        });
+        if (data) api(originalConfig);
+      } catch (error) {
+        return Promise.reject(error);
       }
     }
-
-    return Promise.reject(error);
+    return Promise.reject(err);
   }
 );
 
