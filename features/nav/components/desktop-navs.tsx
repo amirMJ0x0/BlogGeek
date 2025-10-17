7c8c0bfa3c03fda6a33094cf43fcc9160fddcc66
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2025-10-17 18:06:57 +0330

    style: fix isActive for subdomains

--- features/nav/components/desktop-navs.tsx
+++ features/nav/components/desktop-navs.tsx
@@ -7,24 +7,24 @@ import { usePathname } from "next/navigation";
 const DesktopNav = () => {
   const pathname = usePathname();
   return (
-    <div className="max-lg:hidden w-1/6 bg-white dark:bg-[#222831]/70 rounded-2xl p-2 !shadow-sm border-gray-100 max-h-min sticky right-0 top-5">
-      <nav className="flex flex-col justify-center  gap-2">
-        {navigationConstants.map((item) => {
-          const isActive = pathname === item.href;
-          return (
-            <Link href={item.href} key={item.label}>
-              <Button
-                variant={isActive ? "default" : "ghost"}
-                className={`w-full flex gap-3 justify-start`}
-              >
-                <item.logo />
-                {item.label}
-              </Button>
-            </Link>
-          );
-        })}
-      </nav>
-    </div>
+    <nav className="flex flex-col justify-center  gap-2">
+      {navigationConstants.map((item) => {
+        const isActive =
+          pathname === item.href || pathname.startsWith(`${item.href}/`);
+
+        return (
+          <Link href={item.href} key={item.label}>
+            <Button
+              variant={isActive ? "default" : "ghost"}
+              className={`w-full flex gap-3 justify-start`}
+            >
+              <item.logo />
+              {item.label}
+            </Button>
+          </Link>
+        );
+      })}
+    </nav>
   );
 };
 
--- features/nav/constants/index.ts
+++ features/nav/constants/index.ts
@@ -1,13 +1,8 @@
-import { Compass, House, Plus, User, Bookmark } from "lucide-react";
+import { Compass, House, Plus, User } from "lucide-react";
 
-export const MobileNavs = [
+export const navigationConstants = [
   { label: "خانه", logo: House, href: "/" },
   { label: "اکسپلور", logo: Compass, href: "#" },
   { label: "نوشتن", logo: Plus, href: "/write" },
   { label: "پروفایل", logo: User, href: "/profile" },
-];
-
-export const DesktopNavs = [
-  ...MobileNavs,
-  { label: "نشان ها", logo: Bookmark, href: "#" },
 ];
