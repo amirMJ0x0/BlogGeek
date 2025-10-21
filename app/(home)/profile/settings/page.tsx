8ebadad70331b9e658bfc1bcd7a28ddcb7e131cb
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2025-10-21 18:30:09 +0330

    seo: Add metadata to profile setting page

--- app/(home)/profile/settings/page.tsx
+++ app/(home)/profile/settings/page.tsx
@@ -1,6 +1,12 @@
 import { Spinner } from "@/components/ui/spinner";
 import SettingsTabs from "@/features/user/components/profile-setting/settings-tabs";
+import { Metadata } from "next";
 import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "تنظیمات حساب کاربری",
+  description: "تنظیمات حساب کاربری (اطلاعات فردی و حساب کاربری و ...)",
+};
 
 const Settings = () => {
   return (
