949ddb77194ef024d0fe43a1870785e7aea0edaf
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2025-11-05 14:58:53 +0330

    Update about section of profile setting + fix birthday format on first render + remove preAddon for socialmedias + Change input placeholders

--- features/user/components/profile-setting/setting-about-section.tsx
+++ features/user/components/profile-setting/setting-about-section.tsx
@@ -25,6 +25,7 @@ import {
   FieldGroup,
   FieldLabel,
 } from "@/components/ui/field";
+import { format } from "date-fns";
 import { useEditUserInfo } from "../../hooks/useEditUserInfo";
 import { useCustomToast } from "@/features/nav/hooks/useCustomToast";
 import { Spinner } from "@/components/ui/spinner";
@@ -38,7 +39,7 @@ const SettingAboutSection = () => {
   const router = useRouter();
   const form = useForm<ProfileInfoFormData>({
     resolver: zodResolver(profileInfoSchema),
-    defaultValues: {
+    values: {
       first_name: user?.first_name ?? "",
       last_name: user?.last_name ?? "",
       birthday: user?.birthday ?? "",
@@ -55,13 +56,13 @@ const SettingAboutSection = () => {
     const apiData: EditUserInfoRequest = {
       first_name: data.first_name || null,
       last_name: data.last_name || null,
-      birthday: data.birthday || null,
+      birthday: format(data.birthday!, "yyyy-MM-dd") || null,
       bio: data.bio || null,
       social_media: {
         instagram: data.instagram
           ? `https://instagram.com/${data.instagram}`
           : null,
-        twitter: data.twitter ? `https://twitter.com/${data.twitter}` : null,
+        twitter: data.twitter ? `https://x.com/${data.twitter}` : null,
         telegram: data.telegram ? `https://t.me/${data.telegram}` : null,
         linkedin: data.linkedin
           ? `https://linkedin.com/in/${data.linkedin}`
@@ -103,7 +104,7 @@ const SettingAboutSection = () => {
                 <Input
                   {...field}
                   id="first_name"
-                  placeholder="علی"
+                  placeholder="Your First Name"
                   aria-invalid={fieldState.invalid}
                   className="min-h-9"
                   autoComplete="off"
@@ -129,7 +130,7 @@ const SettingAboutSection = () => {
                 <Input
                   {...field}
                   id="last_name"
-                  placeholder="اسدی"
+                  placeholder="Your Last Name"
                   className="min-h-9"
                   aria-invalid={fieldState.invalid}
                   autoComplete="off"
@@ -159,7 +160,7 @@ const SettingAboutSection = () => {
                   <InputGroupTextarea
                     {...field}
                     id="bio"
-                    placeholder="یه جوون ایرانی برنامه نویس..."
+                    placeholder="Tell us about yourself"
                     aria-invalid={fieldState.invalid}
                     rows={3}
                     className="min-h-27 max-h-27 resize-none"
@@ -200,7 +201,7 @@ const SettingAboutSection = () => {
                   <InputGroupInput
                     {...field}
                     id="telegram"
-                    placeholder="aliAsadi1"
+                    placeholder="Link to telegram profile"
                     aria-invalid={fieldState.invalid}
                     autoComplete="off"
                   />
@@ -219,9 +220,6 @@ const SettingAboutSection = () => {
                         />
                       </svg>
                     </InputGroupButton>
-                  </InputGroupAddon>
-                  <InputGroupAddon className="text-muted-foreground pl-1.5">
-                    https://t.me/
                   </InputGroupAddon>
                 </InputGroup>
                 {fieldState.invalid && (
@@ -249,7 +247,7 @@ const SettingAboutSection = () => {
                     aria-invalid={fieldState.invalid}
                     autoComplete="off"
                     id="instagram"
-                    placeholder="aliAsadi1"
+                    placeholder="Link to instagram profile"
                   />
                   <InputGroupAddon>
                     <InputGroupButton variant="secondary" size="icon-xs">
@@ -266,9 +264,6 @@ const SettingAboutSection = () => {
                         />
                       </svg>
                     </InputGroupButton>
-                  </InputGroupAddon>
-                  <InputGroupAddon className="text-muted-foreground pl-1.5">
-                    https://instagram.com/
                   </InputGroupAddon>
                 </InputGroup>
                 {fieldState.invalid && (
@@ -296,7 +291,7 @@ const SettingAboutSection = () => {
                     aria-invalid={fieldState.invalid}
                     autoComplete="off"
                     id="twitter"
-                    placeholder="aliAsadi1"
+                    placeholder="Link to twitter profile"
                   />
                   <InputGroupAddon>
                     <InputGroupButton variant="secondary" size="icon-xs">
@@ -306,9 +301,6 @@ const SettingAboutSection = () => {
                         </g>
                       </svg>
                     </InputGroupButton>
-                  </InputGroupAddon>
-                  <InputGroupAddon className="text-muted-foreground pl-1.5">
-                    https://twitter.com/
                   </InputGroupAddon>
                 </InputGroup>
                 {fieldState.invalid && (
@@ -336,7 +328,7 @@ const SettingAboutSection = () => {
                     aria-invalid={fieldState.invalid}
                     autoComplete="off"
                     id="linkedin"
-                    placeholder="aliAsadi1"
+                    placeholder="Link to linkedin profile"
                   />
                   <InputGroupAddon>
                     <InputGroupButton variant="secondary" size="icon-xs">
@@ -351,9 +343,6 @@ const SettingAboutSection = () => {
                         <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
                       </svg>
                     </InputGroupButton>
-                  </InputGroupAddon>
-                  <InputGroupAddon className="text-muted-foreground pl-1.5">
-                    https://linkedin.com/in/
                   </InputGroupAddon>
                 </InputGroup>
                 {fieldState.invalid && (
@@ -380,7 +369,7 @@ const SettingAboutSection = () => {
                     aria-invalid={fieldState.invalid}
                     autoComplete="off"
                     id="github"
-                    placeholder="aliAsadi1"
+                    placeholder="Link to github profile"
                   />
                   <InputGroupAddon>
                     <InputGroupButton variant="secondary" size="icon-xs">
@@ -396,9 +385,6 @@ const SettingAboutSection = () => {
                         <path d="M18.88 1.099C18.147.366 17.265 0 16.233 0H3.746C2.714 0 1.832.366 1.099 1.099.366 1.832 0 2.714 0 3.746v12.487c0 1.032.366 1.914 1.099 2.647.733.733 1.615 1.099 2.647 1.099H6.66c.19 0 .333-.007.429-.02a.504.504 0 0 0 .286-.169c.095-.1.143-.245.143-.435l-.007-.885c-.004-.564-.006-1.01-.006-1.34l-.3.052c-.19.035-.43.05-.721.046a5.555 5.555 0 0 1-.904-.091 2.026 2.026 0 0 1-.872-.39 1.651 1.651 0 0 1-.572-.8l-.13-.3a3.25 3.25 0 0 0-.41-.663c-.186-.243-.375-.407-.566-.494l-.09-.065a.956.956 0 0 1-.17-.156.723.723 0 0 1-.117-.182c-.026-.061-.004-.111.065-.15.07-.04.195-.059.378-.059l.26.04c.173.034.388.138.643.311a2.1 2.1 0 0 1 .631.677c.2.355.44.626.722.813.282.186.566.28.852.28.286 0 .533-.022.742-.065a2.59 2.59 0 0 0 .585-.196c.078-.58.29-1.028.637-1.34a8.907 8.907 0 0 1-1.333-.234 5.314 5.314 0 0 1-1.223-.507 3.5 3.5 0 0 1-1.047-.872c-.277-.347-.505-.802-.683-1.365-.177-.564-.266-1.215-.266-1.952 0-1.049.342-1.942 1.027-2.68-.32-.788-.29-1.673.091-2.652.252-.079.625-.02 1.119.175.494.195.856.362 1.086.5.23.14.414.257.553.352a9.233 9.233 0 0 1 2.497-.338c.859 0 1.691.113 2.498.338l.494-.312a6.997 6.997 0 0 1 1.197-.572c.46-.174.81-.221 1.054-.143.39.98.424 1.864.103 2.653.685.737 1.028 1.63 1.028 2.68 0 .737-.089 1.39-.267 1.957-.177.568-.407 1.023-.689 1.366-.282.343-.633.63-1.053.865-.42.234-.828.403-1.223.507a8.9 8.9 0 0 1-1.333.235c.45.39.676 1.005.676 1.846v3.11c0 .147.021.266.065.357a.36.36 0 0 0 .208.189c.096.034.18.056.254.064.074.01.18.013.318.013h2.914c1.032 0 1.914-.366 2.647-1.099.732-.732 1.099-1.615 1.099-2.647V3.746c0-1.032-.367-1.914-1.1-2.647z" />
                       </svg>
                     </InputGroupButton>
-                  </InputGroupAddon>
-                  <InputGroupAddon className="text-muted-foreground pl-1.5">
-                    https://github.com/
                   </InputGroupAddon>
                 </InputGroup>
                 {fieldState.invalid && (
