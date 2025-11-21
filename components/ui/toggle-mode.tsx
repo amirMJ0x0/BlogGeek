c6a7cfc5c08b009e3484466976ebff337fe96ad6
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2025-11-22 00:09:19 +0330

    style: Improve ux of toggle theme button in header

--- components/ui/toggle-mode.tsx
+++ components/ui/toggle-mode.tsx
@@ -19,9 +19,9 @@ export function ModeToggle() {
     <DropdownMenu dir="rtl">
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon" title="toggle theme color">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
+          <Sun className="!h-[1.2rem] !w-[1.2rem] !scale-100 !rotate-0 !transition-all dark:!scale-0 dark:!-rotate-90" />
+          <Moon className="!absolute !h-[1.2rem] !w-[1.2rem] !scale-0 !rotate-90 !transition-all dark:!scale-100 dark:!rotate-0" />
+          <span className="!sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
