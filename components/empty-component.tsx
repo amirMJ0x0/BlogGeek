36803cad59e909af6d511e8eadf2f0ecd55c36b5
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2025-12-01 20:40:56 +0330

    feat: Implement dynamic empty-component to show 'There is no post' message or etc

---
+++ components/empty-component.tsx
@@ -0,0 +1,18 @@
+import { FolderOpen, LucideIcon } from "lucide-react";
+import { ReactNode } from "react";
+
+type props = {
+  message?: string;
+  icon?: ReactNode;
+};
+
+const EmptyComponent = ({ message, icon }: props) => {
+  return (
+    <div className="flex flex-col justify-center h-48 items-center p-6 gap-2 text-center text-gray-500">
+      {icon ? icon : <FolderOpen className="size-10" />}
+      {message ? message : "پستی وجود ندارد"}
+    </div>
+  );
+};
+
+export default EmptyComponent;
