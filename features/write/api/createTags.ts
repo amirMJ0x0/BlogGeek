fde8b0bf73fca27737bd5d37b970ceccb7d90858
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2025-12-22 05:52:46 +0330

    fix: solve build time error in createTags function

--- features/write/api/createTags.ts
+++ features/write/api/createTags.ts
@@ -1,11 +1,11 @@
 import api from "@/lib/api";
 import { ApiResponse } from "@/types";
 import { TagsCreateRequest } from "../types";
-import { TagItem } from "@/features/tags";
+import { TagItemType } from "@/features/tags";
 
 export const createTags = async (data: TagsCreateRequest) => {
   try {
-    const response = await api.post<ApiResponse<TagItem>>("v1/tag", data);
+    const response = await api.post<ApiResponse<TagItemType>>("v1/tag", data);
     return response.data.data;
   } catch (error) {
     throw error;
