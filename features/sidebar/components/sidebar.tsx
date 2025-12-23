92a18e9a3efb814422fd57e087df57732b00c592
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2025-12-23 16:43:59 +0330

    feat(sidebar): add loading spinner and improve user display in sidebar

--- features/sidebar/components/sidebar.tsx
+++ features/sidebar/components/sidebar.tsx
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import FollowButton from "@/features/user/components/profile/follow-button";
 import { Users } from "lucide-react";
+import { Spinner } from "@/components/ui/spinner";
 
 const SideBar = () => {
   const pathname = usePathname();
@@ -20,41 +21,56 @@ const SideBar = () => {
   });
 
   return (
-    <div className="max-lg:hidden w-1/5 bg-white dark:!bg-secondary-dark rounded-2xl p-4 flex flex-col items-start gap-7 !h-screen sticky left-0 top-5 ">
+    <div className="max-lg:hidden w-1/5 bg-white dark:!bg-secondary-dark rounded-2xl p-4 !shadow-sm mx-auto gap-4 !h-screen sticky left-0 top-5 ">
       <h3 className="font-bold flex lg:gap-1 xl:gap-2 items-center lg:text-sm xl:text-md !tracking-tighter">
         <Users className="size-[1.3rem]" /> نویسنده های پیشنهادی
       </h3>
-      {data?.map((user) => {
-        return (
-          <div key={user?.id} className="flex justify-between w-full">
-            <div className="flex gap-2 items-center">
-              <Avatar>
-                <AvatarImage src={user?.profile_image || ""} />
-                <AvatarFallback className="bg-secondary-light dark:bg-secondary-dark">
-                  {/* {user?.first_name
-                      ? user?.first_name.substring(0, 1)
-                      : user?.username.substring(0, 1) || "U"} */}
-                </AvatarFallback>
-              </Avatar>
-              <h4 className="text-sm font-semibold ">
-                {user?.first_name || user?.last_name
-                  ? `${user?.first_name ?? ""} ${user?.last_name ?? ""}`.trim()
-                  : user?.username}
-              </h4>
-            </div>
-            <FollowButton
-              userId={user.id}
-              followOptions={{
-                is_followed_by_you: user.is_followed_by_you,
-                is_following: user.is_following,
-              }}
-              className="text-xs gap-0 !py-0 "
-              hasIcon={false}
-              size={"sm"}
-            />
+      <div className="flex-col gap-2 mt-2">
+        {isLoading ? (
+          <div className="mx-auto">
+            <Spinner />
           </div>
-        );
-      })}
+        ) : (
+          data?.map((user) => {
+            return (
+              <div key={user?.id} className="w-full">
+                <div className="flex gap-2 items-center w-full">
+                  <Avatar className="size-12">
+                    <AvatarImage src={user?.profile_image || ""} />
+                    <AvatarFallback className="bg-secondary-light dark:bg-secondary-dark">
+                      {user?.first_name
+                        ? user?.first_name.substring(0, 1)
+                        : user?.username.substring(0, 1)}
+                    </AvatarFallback>
+                  </Avatar>
+                  <div className="flex-col items-center mt-2">
+                    <h4 className="text-md font-semibold text-justify">
+                      {user?.first_name || user?.last_name
+                        ? `${user?.first_name ?? ""} ${
+                            user?.last_name ?? ""
+                          }`.trim()
+                        : user?.username}
+                    </h4>
+                    <div className="w-full justify-end -mt-1 mr-1">
+                      <FollowButton
+                        userId={user.id}
+                        followOptions={{
+                          is_followed_by_you: user.is_followed_by_you,
+                          is_following: user.is_following,
+                        }}
+                        className="text-sm gap-0 p-0 text-blue-600 "
+                        // hasIcon={false}
+                        variant={"link"}
+                        size={"sm"}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        )}
+      </div>
     </div>
   );
 };
