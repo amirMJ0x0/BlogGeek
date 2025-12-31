0d4409e01463e1de581f8904a643e35f2886e806
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2026-01-01 02:22:00 +0330

    feat(getBlogById): remove ouput type of that

--- features/blogs/api/getBlogById.ts
+++ features/blogs/api/getBlogById.ts
@@ -1,7 +1,7 @@
 import api from "@/lib/client/api";
 import { Blog } from "../blogTypes";
 
-export const getBlogById = async (id: number): Promise<Blog> => {
+export const getBlogById = async (id: number) => {
   const res = await api.get(`v1/blog/${id}`);
   return res.data.data;
 };
